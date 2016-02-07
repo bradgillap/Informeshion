1e51ded9a0571d5d00fbbec657a38cad607927ba
Bradley Gillap <bradgillap@gmail.com>
2016-02-07 00:33:35 -0500

    Create installscript.sh
    Started some work on the install script.

---
+++ autoinstall/installscript.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#######VARIABLES######
+
+STR="Hello World!"
+echo $STR
+
+# This is a  substitution variable. It assigns the output of a command to a variable
+# Shows disk space in whiptail window
+space=$(df -H /)
+
+
+####### ROOT CHECK ######
+# Are we root? If not use sudo
+SUDO=''
+if (( $EUID != 0 )); then
+    SUDO='sudo'
+fi
+
+
+######## UPDATE RASPBIAN #######
+
+#$SUDO apt-get update
+#$SUDO apt-get upgrade -y
+
+
+####### Configure Pi? #######
+if (whiptail --title "" --yesno "Do you need to run Raspi-Config to expand your SDCard size? If you choose yes, you will need to restart the process. \n Current Partition size is \n \n $space"  20 90 ) then
+   echo "Yes $?."
+   $SUDO raspi-config
+Broadcast message from pi@raspberrypi on pts/1 (Sun 2016-02-07 00:27:21 EST):
+    echo "No $?."
+
+
+#END OF FILE
