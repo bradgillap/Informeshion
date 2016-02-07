b4d6a2912ceffdcfcf321a07d6daee901e0e92f5
bradgillap <bradgillap@gmail.com>
2016-02-07 04:09:11 -0500

    More Dialogue changes

--- autoinstall/installscript.sh
+++ autoinstall/installscript.sh
@@ -20,12 +20,16 @@ fi
 
 ######## UPDATE RASPBIAN #######
 
-#$SUDO apt-get update
-#$SUDO apt-get upgrade -y
+if (whiptail --title "Update Raspbian" --yes-button "Update" --no-button "Skip"  --yesno "We must update packages repos and packages before we begin." 20 90 ) then
 
+$SUDO apt-get update
+$SUDO apt-get upgrade -y
+else
+echo "$?"
+fi
 
-####### Configure Pi? #######
-if (whiptail --title "" --yesno "Do you need to run Raspi-Config to expand your SDCard size? \n \n Current Partition size is: \n \n $space"  20 90 ) then
+####### Configure PI RASP-CONFIG #######
+if (whiptail --title "Run Raspi-Config?" --no-button "Skip" --yesno "Do you need to run Raspi-Config to expand your SDCard size? \n \n Current Partition size is: \n \n $space"  20 90 ) then
    echo "Yes $?."
    $SUDO raspi-config
 else
@@ -33,7 +37,7 @@ else
 fi
 
 
-if (whiptail --title "" --yesno "We will now install the required packages. Continue?"  20 90 ) then
+if (whiptail --title "Install Needed Packages?" --yesno "We will now install the required packages. Continue?"  20 90 ) then
 
 ########INSTALL SOME STUFF #######
 
