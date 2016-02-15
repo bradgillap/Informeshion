faaa1128b1a372c8146abbf6d8eee5c25f5beecf
bradgillap <bradgillap@gmail.com>
2016-02-15 01:40:22 -0500

    Cant figure out how to strip double quotes from app install

--- autoinstall/install.sh
+++ autoinstall/install.sh
@@ -1,1 +1,48 @@
+#! /bin/bash
 
+## Time for a rewrite
+## Two menu tracks this time. One beginner and one advanced
+
+## INITIALIZE VARIABLES ##
+
+SPACE=$(df -H /)
+INSTPACKAGES=""
+
+
+## ROOT CHECK ## 
+# Are we root? If not use sudo
+SUDO=''
+if (( $EUID != 0 )); then
+    SUDO='sudo'
+    echo "Not logged in as root, using sudo"
+fi
+
+## INTERNET CHECK ##
+
+wget -q --spider http://google.com
+
+if [ $? -eq 0 ]; then
+    echo "Okay we are connected to the Internet. Let's proceed"
+else
+    echo "Plase connect to the Internet with cable and try running again"
+    exit 1
+fi
+
+
+
+## ADV SOFTWARE INSTALL ##
+INSTPACKAGES=$(whiptail --title "Advanced - Choose Packages" --checklist \
+"Choose packages to install. Required packages are already selected." 15 60 4 \
+"batctl" "Configure mesh layer 2 protocol." ON \
+"dnsutils" "Includes DNS query tools like dig." OFF \
+"dnsmasq" "DHCP and DNS server in one package." OFF 3>&1 1>&2 2>&3)
+ 
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Installing selected packagese"
+
+    $SUDO apt-get install $INSTPACKAGES | tr -d '"' 
+
+else
+    echo "You chose Cancel."
+fi
