c272b441adbc630ac4b6958b9c2d5c43f18d310e
bradgillap <bradgillap@gmail.com>
2016-02-16 14:56:18 -0500

    improved root check. Thanks pi-hole project

--- autoinstall/install.sh
+++ autoinstall/install.sh
@@ -13,10 +13,18 @@
 
 ## ROOT CHECK ## 
 # Are we root? If not use sudo
-SUDO=''
-if (( $EUID != 0 )); then
-    SUDO='sudo'
-    echo "Not logged in as root, using sudo"
+if [[ $EUID -eq 0 ]];then
+    echo "You are root."
+else
+    echo "Sudo will be used for the install."
+    # Check if it is actually installed
+    # If it isn't, exit because the install cannot complete
+    if [[ $(dpkg-query -s sudo) ]];then
+        export SUDO="sudo"
+    else
+        echo "Please install sudo or run this as root."
+        exit 1
+    fi
 fi
 
 ## INTERNET CHECK ##
