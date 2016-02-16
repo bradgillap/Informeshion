348d106cf527b518adbf1c6e2a1c8478221eca34
bradgillap <bradgillap@gmail.com>
2016-02-15 21:01:10 -0500

    Fleshing out menus using functions and cases so it makes sense.

--- autoinstall/install.sh
+++ autoinstall/install.sh
@@ -6,8 +6,8 @@
 ## INITIALIZE VARIABLES ##
  SPACE=$(df -H /)
  INSTPACKAGES=""
-
-
+ MAINSEL=""
+ exitstatus=""
 ## ROOT CHECK ## 
 # Are we root? If not use sudo
 SUDO=''
@@ -28,21 +28,67 @@ else
 fi
 
 
+## FUNCTIONS ##
 
-## ADV SOFTWARE INSTALL ##
-INSTPACKAGES=$(whiptail --title "Advanced - Choose Packages" --checklist \
-"Choose packages to install. Required packages are already selected." 15 60 4 \
-"batctl" "Configure mesh layer 2 protocol." ON \
-"dnsutils" "Includes DNS query tools like dig." ON \
-"dnsmasq" "DHCP and DNS server in one package." OFF 3>&1 1>&2 2>&3)
- 
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    echo "Installing selected packagese"
-    INSTPACKAGES=$(echo "$INSTPACKAGES" | tr -d '"') #Removed double quotes from selection output.
-    $SUDO apt-get install $INSTPACKAGES
 
-else
-    echo "You chose Cancel."
-fi
 
+
+function SimpleInstall() {
+    $SUDO apt-get update && $SUDO apt-get upgrade -y
+    $SUDO apt-get install batctl dnsutils dnsmasq -y
+    $SUDO modprobe batman-adv
+}
+
+
+function AdvancedInstall() {
+    PKGSEL=$(whiptail --checklist --title "Select Packages" 15 60 4 \
+        "batctl"   "Configure mesh layer 2 protocol." ON \
+        "dnsutils" "Includes DNS query tools like dig." ON \
+        "dnsmasq"  "DHCP and DNS server in one package." ON 3>&1 1>&2 2>&3)
+    exitstatus=$?
+        if [ $exitstatus = 0 ]; then
+            echo "Installing selected packagese"
+            INSTPACKAGES=$(echo "$INSTPACKAGES" | tr -d '"') #Removed double quotes from selection output.
+            $SUDO apt-get install $INSTPACKAGES
+        else
+            echo "You chose Cancel."
+            MainMenu
+        fi
+}
+
+
+function AdvancedMenu() {
+    ADVSEL=$(whiptail --title "Advanced Menu" --menu "Choose an option" 15 60 4 \
+        "1" "Choose Packages to Install" \
+        "2" "Something else" 3>&1 1>&2 2>&3)
+    case $ADVSEL in
+        1)
+            echo "User selected Package install."
+            AdvancedInstall
+        ;;
+        2)
+            echo "User selected something else."
+        ;;
+    esac
+}
+function MainMenu() {
+    MAINSEL=$(whiptail --title "Main Menu" --menu "Choose your option" 15 60 4 \
+        "1" "Automatic Install" \
+        "2" "Manual Install"  3>&1 1>&2 2>&3)
+
+        case $MAINSEL in
+
+            1)
+                echo "User selected automatic install"
+                SimpleInstall
+            ;;
+            2)
+                echo "User selected advanced install"
+                AdvancedMenu
+            ;; 
+        esac
+}
+
+
+
+MainMenu
